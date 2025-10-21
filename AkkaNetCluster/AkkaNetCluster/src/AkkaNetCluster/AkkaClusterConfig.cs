0557fee242d2ba05b9fd39498dc1a35fd16d0942
Nikolay Portsel <Portsel.NA@gazprom-neft.ru>
2025-10-21 16:55:59 +0300

    init from PetabridgeTemplate

---
+++ AkkaNetCluster/AkkaNetCluster/src/AkkaNetCluster/AkkaClusterConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace AkkaNetCluster
+{
+    /// <summary>
+    /// To be parsed from appsettings.json or environment variables
+    /// </summary>
+    public class AkkaClusterConfig
+    {
+        public string ActorSystemName { get; set; } = "ActorSystem";
+        public string? Hostname { get; set; }
+        public int? Port { get; set; }
+
+        public string[]? Roles { get; set; } = Array.Empty<string>();
+
+        public string[]? SeedNodes { get; set; } = Array.Empty<string>();
+    }
+}
---
+++ AkkaNetCluster/AkkaNetCluster/src/AkkaNetCluster/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using Akka.Actor;
+using Akka.Cluster.Hosting;
+using Akka.Hosting;
+using Akka.Remote.Hosting;
+using AkkaNetCluster;
+using Petabridge.Cmd.Cluster;
+using Petabridge.Cmd.Cluster.Sharding;
+using Petabridge.Cmd.Host;
+using Petabridge.Cmd.Remote;
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{environment}.json", optional: true)
+    .AddEnvironmentVariables();
+
+builder.Logging.ClearProviders().AddConsole();
+
+var akkaConfig = builder.Configuration.GetRequiredSection(nameof(AkkaClusterConfig))
+    .Get<AkkaClusterConfig>();
+
+builder.Services.AddControllers();
+builder.Services.AddAkka(akkaConfig.ActorSystemName, (builder, provider) =>
+{
+    Debug.Assert(akkaConfig.Port != null, "akkaConfig.Port != null");
+    builder.AddHoconFile("app.conf", HoconAddMode.Append)
+        .WithRemoting(akkaConfig.Hostname, akkaConfig.Port.Value)
+        .WithClustering(new ClusterOptions()
+        {
+            Roles = akkaConfig.Roles,
+            SeedNodes = akkaConfig.SeedNodes
+        })
+        .AddPetabridgeCmd(cmd =>
+        {
+            cmd.RegisterCommandPalette(new RemoteCommands());
+            cmd.RegisterCommandPalette(ClusterCommands.Instance);
+
+            // sharding commands, although the app isn't configured to host any by default
+            cmd.RegisterCommandPalette(ClusterShardingCommands.Instance);
+        })
+        .WithActors((system, registry) =>
+        {
+            var consoleActor = system.ActorOf(Props.Create(() => new ConsoleActor()), "console");
+            registry.Register<ConsoleActor>(consoleActor);
+        });
+});
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+
+    endpoints.MapGet("/", async (HttpContext context, ActorRegistry registry) =>
+    {
+        var reporter = registry.Get<ConsoleActor>();
+        var resp = await reporter.Ask<string>($"hit from {context.TraceIdentifier}", context.RequestAborted); // calls Akka.NET under the covers
+        await context.Response.WriteAsync(resp);
+    });
+});
+
+
+await app.RunAsync();
